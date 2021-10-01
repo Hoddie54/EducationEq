b4656e1735e72364fee8e842b0c9e711abcf8899
Amir Hamza <amir@educationequation.co.uk>
2021-10-01 12:17:41 +0100

    Adding index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
+import history from "./history"
+import { Provider } from "react-redux"
+import { store, persistor } from "./utils/redux/store"
+import { PersistGate } from "redux-persist/integration/react"
+import SpinnerPage from "./pages/spinner/spinner.component"
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter history={history}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+)
